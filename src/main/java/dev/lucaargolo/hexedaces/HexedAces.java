effdb0711757b9352fdd819b30bb76671d78dd17
lucaargolo <lucaargolo@gmail.com>
2024-10-28 16:31:59 -0300

    Initial commit.

---
+++ src/main/java/dev/lucaargolo/hexedaces/HexedAces.java
@@ -0,0 +1,22 @@
+package dev.lucaargolo.hexedaces;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(HexedAces.MODID)
+public class HexedAces {
+
+    public static final String MODID = "hexedaces";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public HexedAces(IEventBus modEventBus, ModContainer modContainer) {
+
+
+    }
+
+}
