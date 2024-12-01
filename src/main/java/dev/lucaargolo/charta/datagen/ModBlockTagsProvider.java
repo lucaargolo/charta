dd1e416d44284a6326219c2a8e366dc5fad87693
lucaargolo <lucaargolo@gmail.com>
2024-12-01 01:02:46 -0300

    Sort block tag list so it isn't always changing every commit.

--- src/main/java/dev/lucaargolo/charta/datagen/ModBlockTagsProvider.java
+++ src/main/java/dev/lucaargolo/charta/datagen/ModBlockTagsProvider.java
@@ -3,11 +3,9 @@ package dev.lucaargolo.charta.datagen;
 import dev.lucaargolo.charta.Charta;
 import dev.lucaargolo.charta.block.ModBlocks;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.IntrinsicHolderTagsProvider;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -16,9 +14,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
 
 public class ModBlockTagsProvider extends IntrinsicHolderTagsProvider<Block> {
 
@@ -29,12 +27,12 @@ public class ModBlockTagsProvider extends IntrinsicHolderTagsProvider<Block> {
 
     @Override
     protected void addTags(@NotNull HolderLookup.Provider provider) {
-        List<Block> mineableWithAxe = new ArrayList<>();
-        mineableWithAxe.addAll(ModBlocks.CARD_TABLE_MAP.values().stream().map(DeferredHolder::get).toList());
-        mineableWithAxe.addAll(ModBlocks.GAME_CHAIR_MAP.values().stream().map(DeferredHolder::get).toList());
-        mineableWithAxe.addAll(ModBlocks.BAR_STOOL_MAP.values().stream().map(DeferredHolder::get).toList());
-        mineableWithAxe.addAll(ModBlocks.BAR_SHELF_MAP.values().stream().map(DeferredHolder::get).toList());
-        tag(BlockTags.MINEABLE_WITH_AXE)
-            .add(mineableWithAxe.toArray(new Block[0]));
+        List<DeferredHolder<Block, ? extends Block>> mineableWithAxe = new ArrayList<>();
+        mineableWithAxe.addAll(ModBlocks.CARD_TABLE_MAP.values());
+        mineableWithAxe.addAll(ModBlocks.GAME_CHAIR_MAP.values());
+        mineableWithAxe.addAll(ModBlocks.BAR_STOOL_MAP.values());
+        mineableWithAxe.addAll(ModBlocks.BAR_SHELF_MAP.values());
+        mineableWithAxe.sort(Comparator.comparing(DeferredHolder::getId));
+        tag(BlockTags.MINEABLE_WITH_AXE).addAll(mineableWithAxe.stream().map(DeferredHolder::getKey).toList());
     }
 }
