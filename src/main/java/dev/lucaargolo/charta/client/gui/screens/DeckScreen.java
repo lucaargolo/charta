770172fc893d3a4dc6ec549bd02b7a8b6edcce75
lucaargolo <lucaargolo@gmail.com>
2025-05-25 01:42:49 -0300

    Remove this since it's already calculated in the deck.

--- src/main/java/dev/lucaargolo/charta/client/gui/screens/DeckScreen.java
+++ src/main/java/dev/lucaargolo/charta/client/gui/screens/DeckScreen.java
@@ -18,15 +18,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DeckScreen extends CardScreen implements HoverableRenderable {
 
     private final Screen parent;
 
     private final CardDeck deck;
-    private final Set<Suit> suits;
 
     private int headerOffset = 10;
     private int footerOffset = 10;
@@ -35,7 +32,6 @@ public class DeckScreen extends CardScreen implements HoverableRenderable {
         super(deck.getName());
         this.parent = parent;
         this.deck = deck;
-        this.suits = this.deck.getCards().stream().map(Card::suit).collect(Collectors.toSet());
     }
 
     @Override
@@ -53,14 +49,14 @@ public class DeckScreen extends CardScreen implements HoverableRenderable {
         float cardHeight = CardSlot.getHeight(CardSlot.Type.DEFAULT);
         float maxTopOffset = cardHeight + cardHeight/10f;
 
-        float topOffset = cardHeight + Math.max(0f, maxHeight - (suits.size() * cardHeight)/(float) suits.size());
-        float totalHeight = cardHeight + (topOffset * (suits.size() - 1f));
+        float topOffset = cardHeight + Math.max(0f, maxHeight - (deck.getUniqueSuits().size() * cardHeight)/(float) deck.getUniqueSuits().size());
+        float totalHeight = cardHeight + (topOffset * (deck.getUniqueSuits().size() - 1f));
         float topExcess = totalHeight - maxHeight;
         if(topExcess > 0) {
-            topOffset -= topExcess / (suits.size() - 1f);
+            topOffset -= topExcess / (deck.getUniqueSuits().size() - 1f);
         }
 
-        totalHeight = cardHeight + (maxTopOffset * (suits.size() - 1f));
+        totalHeight = cardHeight + (maxTopOffset * (deck.getUniqueSuits().size() - 1f));
         float top = 0;
         if(topOffset > maxTopOffset) {
             top = Math.max(topOffset - maxTopOffset, (maxHeight - totalHeight));
@@ -71,7 +67,7 @@ public class DeckScreen extends CardScreen implements HoverableRenderable {
         footerOffset = Math.max(0, Mth.floor(top/2 + 27));
 
         int i = 0;
-        for(Suit suit : suits) {
+        for(Suit suit : deck.getUniqueSuits()) {
             List<Card> cards = deck.getCards().stream().filter(c -> c.suit().equals(suit)).sorted().toList();
 
             float leftOffset = cardWidth + Math.max(0f, maxWidth - (cards.size() * cardWidth)/(float) cards.size());
@@ -108,12 +104,12 @@ public class DeckScreen extends CardScreen implements HoverableRenderable {
     protected void renderFg(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY) {
         guiGraphics.drawCenteredString(font, title, width/2, headerOffset, 0xFFFFFFFF);
         int i = 0;
-        int totalWidth = suits.size()*16 - 3;
-        for(Suit suit : suits) {
+        int totalWidth = deck.getUniqueSuits().size()*16 - 3;
+        for(Suit suit : deck.getUniqueSuits()) {
             ChartaGuiGraphics.blitSuitAndGlow(guiGraphics, deck, suit, width/2f - totalWidth/2f + (i*16), headerOffset+10, 0, 0, 13, 13, 13, 13);
             i++;
         }
-        guiGraphics.drawCenteredString(font, Component.literal(deck.getCards().size() + " ").append(Component.translatable("charta.cards")).append(" | "+suits.size()+" ").append(Component.translatable("charta.suits")), width/2, height-footerOffset, 0xFFFFFFFF);
+        guiGraphics.drawCenteredString(font, Component.literal(deck.getCards().size() + " ").append(Component.translatable("charta.cards")).append(" | "+deck.getUniqueSuits().size()+" ").append(Component.translatable("charta.suits")), width/2, height-footerOffset, 0xFFFFFFFF);
     }
 
     @Override
